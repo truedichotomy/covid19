4ceaca4c28bf9dccc764cd9c0a14439dd4117f6b
Donglai Gong <gong@vims.edu>
2020-04-24 01:54:33 -0400

    updated data path for COVID19.jl

--- COVID19.jl
+++ COVID19.jl
@@ -1,24 +1,30 @@
 module COVID19
 # This code loads COVID19 data from https://github.com/CSSEGISandData/COVID-19
 #
-# https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv
-# https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv
-# https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv
+# https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_time_series
 
 using CSV, HTTP, DataFrames, Dates
 
 export confirmed, death, recovered, t, lat, lon, country, region
 
 function load_covid19()
-    covid19url_confirmed = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"
-    covid19url_death = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv"
-    covid19url_recovered = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv"
+    covid19url_confirmed_US = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv"
+    covid19url_confirmed_global = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
+    covid19url_death_US = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv"
+    covid19url_death_global = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
+    covid19url_recovered_global = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv"
 
-    covid19path_confirmed = ENV["HOME"] * "/Box/Data/COVID19/covid19_confirmed.csv"
-    covid19path_death = ENV["HOME"] * "/Box/Data/COVID19/covid19_death.csv"
-    covid19path_recovered = ENV["HOME"] * "/Box/Data/COVID19/covid19_recovered.csv"
+    covid19path_confirmed_US = ENV["HOME"] * "/Box/Data/COVID19/covid19_confirmed_US.csv"
+    covid19path_confirmed_global = ENV["HOME"] * "/Box/Data/COVID19/covid19_confirmed_global.csv"
+    covid19path_death_US = ENV["HOME"] * "/Box/Data/COVID19/covid19_death_US.csv"
+    covid19path_death_global = ENV["HOME"] * "/Box/Data/COVID19/covid19_death_global.csv"
+    covid19path_recovered_global = ENV["HOME"] * "/Box/Data/COVID19/covid19_recovered_global.csv"
 
-    io = open(covid19path_confirmed, "w")
+    download(covid19url_confirmed_US, covid19path_confirmed_US)
+    download(covid19url_confirmed_global, covid19path_confirmed_global)
+
+
+#=     io = open(covid19path_confirmed, "w")
     r = HTTP.request("GET", covid19url_confirmed, response_stream=io)
     close(io)
     io = open(covid19path_death, "w")
@@ -26,23 +32,25 @@ function load_covid19()
     close(io)
     io = open(covid19path_recovered, "w")
     r = HTTP.request("GET", covid19url_recovered, response_stream=io)
-    close(io)
+    close(io) =#
 
-    confirmed = CSV.read(covid19path_confirmed);
-    death = CSV.read(covid19path_death);
-    recovered = CSV.read(covid19path_recovered);
+    confirmed_US = CSV.read(covid19path_confirmed_US);
+    death_US = CSV.read(covid19path_death_US);
+    confirmed_global = CSV.read(covid19path_confirmed_global);
+    death_global = CSV.read(covid19path_death_global);
+    recovered_global = CSV.read(covid19path_recovered_global);
 
-    return confirmed, death, recovered
+    return confirmed_US, death_US, confirmed_global, death_global, recovered_global
 end
-(confirmed,death,recovered) = load_covid19()
+(confirmed_US,death_US, confirmed_global, death_global, recovered_global) = load_covid19()
 
 t0 = Date("2020-01-22")
 ndays = Dates.today() - t0;
 t = collect(t0 : Day(1) : Dates.today());
 
-lat = confirmed.Lat;
-lon = confirmed.Long;
-country = confirmed[:,2];
-region = confirmed[:,3];
+lat = confirmed_global.Lat;
+lon = confirmed_global.Long;
+country = confirmed_global[:,2];
+region = confirmed_global[:,3];
 
 end
